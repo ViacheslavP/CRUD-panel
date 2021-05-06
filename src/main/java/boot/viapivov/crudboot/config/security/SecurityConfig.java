380b4c77e7280db9566a5e7ffea5b97a15f609af
ViacheslavP <public.viacheslav@gmail.com>
2021-05-06 16:45:17 +0300

    Moved security v2

--- src/main/java/boot/viapivov/crudboot/config/security/SecurityConfig.java
+++ src/main/java/boot/viapivov/crudboot/config/security/SecurityConfig.java
@@ -1,4 +1,4 @@
-package boot.viapivov.crudboot.service.security;
+package boot.viapivov.crudboot.config.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
--- src/main/java/boot/viapivov/crudboot/controller/UserController.java
+++ src/main/java/boot/viapivov/crudboot/controller/UserController.java
@@ -66,4 +66,5 @@ public class UserController {
         userService.removeById(id);
         return "redirect:/admin";
     }
+
 }
